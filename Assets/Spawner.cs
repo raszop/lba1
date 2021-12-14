dc6496e44fbaa47b8f83c886202bb8ccba28f945
raszop <dawidpoleszczuk@gmail.com>
2021-12-14 18:34:09 +0100

    update

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,18 +4,67 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject objectToSpawn;
+    //public GameObject objectToSpawn;
+    public List<GameObject> objectToSpawn;
 
     public Vector3 spawnPosition;
 
-    public void Spawn()
+    private void Start()
     {
-        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+        StartCoroutine(SpawnRoutine());
+        //Spawn();
     }
 
-    private void Update()
+    private IEnumerator SpawnRoutine()
     {
-        //Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
-        Spawn();
+        for (int licznik = 0; licznik < 10; licznik++)
+        {
+            yield return new WaitForSeconds(2f);
+
+            float x = Random.Range(0f, 20f);
+            float z = Random.Range(0f, 20f);
+
+            spawnPosition = new Vector3(x, 2, z);
+
+            GameObject enemy;
+            int liczba = Random.Range(0, 2);
+
+            enemy = objectToSpawn[liczba];
+
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    public void Spawn()
+    {
+        //int licznik = 0;
+
+        //do
+        //{
+            //licznik += 1;
+
+            //float x = Random.Range(0f, 20f);
+            //float z = Random.Range(0f, 20f);
+
+            //spawnPosition = new Vector3(x, 2, z);
+
+            //Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+
+        //} while (licznik < 10);
+
+        for (int licznik = 0; licznik < 10; licznik++)
+        {
+            float x = Random.Range(0f, 20f);
+            float z = Random.Range(0f, 20f);
+
+            spawnPosition = new Vector3(x, 2, z);
+
+            GameObject enemy;
+            int liczba = Random.Range(0, 2);
+
+            enemy = objectToSpawn[liczba];
+
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }
     }
 }
