dc9d09164bf73910efc05445b88b92d7e8155a2d
BLUEMAN <25blueman25@gmail.com>
2022-03-01 18:25:55 +0100

    Enemy_Saw Update

--- Assets/enemy/Enemy_Script.cs
+++ Assets/enemy/Enemy_Script.cs
@@ -7,26 +7,29 @@ public class Enemy_Script : MonoBehaviour
     public bool Can_Rotate = false;
     public bool Clockwise = false;
     public float Rotation_Speed = 0;
+    private float Current_Rotation = 0;
     // Start is called before the first frame update
     void Start()
     {
-       
-           
-        
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Current_Rotation += Rotation_Speed * Time.deltaTime;
         if (Can_Rotate == true)
         {
             if (Clockwise == true)
             {
-                gameObject.transform.Rotate(0, 0, Rotation_Speed*Time.deltaTime);
+                gameObject.transform.rotation = Quaternion.Euler(new Vector3(-90, 0, Current_Rotation));
+                //gameObject.transform.Rotate(0, 0, Rotation_Speed * Time.deltaTime);
             }
             else
             {
-                gameObject.transform.Rotate(0, 0, -Rotation_Speed*Time.deltaTime);
+                //gameObject.transform.Rotate(0, 0, -Rotation_Speed * Time.deltaTime);
             }
         }
     }
