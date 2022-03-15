75c1cf60fa28325ef55415498d24f604a18c9dd3
BLUEMAN <25blueman25@gmail.com>
2022-03-15 18:39:03 +0100

    Main Menu
    music added

--- Assets/Main_Menu/Main_Menu_Script.cs
+++ Assets/Main_Menu/Main_Menu_Script.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Main_Menu_Script : MonoBehaviour
 {
+    public Toggle volumeToggle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        volumeToggle.onValueChanged.AddListener(ChangeVolume);
     }
 
     // Update is called once per frame
@@ -17,6 +19,16 @@ public class Main_Menu_Script : MonoBehaviour
         
     }
 
+    private void OnDestroy()
+    {
+        volumeToggle.onValueChanged.RemoveListener(ChangeVolume);
+    }
+
+    public void ChangeVolume(bool enabled)
+    {
+        Debug.Log("SOUND IS: " + enabled.ToString());
+        AudioListener.volume = enabled ? 1f : 0f;
+    }
     // On click Play_Button event
     public void Play_Button_On_Click() 
     {
@@ -25,11 +37,11 @@ public class Main_Menu_Script : MonoBehaviour
     }
 
     // On click Options_Button event
-    public void Options_Button_On_Click()
+    /*public void Options_Button_On_Click()
     {
         Debug.Log("OP");
     }
-
+    */
     // On click Exit_Button event
     public void Exit_Button_On_Click()
     {
