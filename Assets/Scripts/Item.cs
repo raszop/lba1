c9d7cce7d96ad819e934a8bfba6d9938a973fffb
dp <dawidpoleszczuk@gmail.com>
2022-05-24 18:35:54 +0200

    bugfix

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -25,7 +25,7 @@ public class Item : MonoBehaviour
                 player.Heal(healHpValue);
                 player.maxHp += additionalHealth;
                 player.damage += additionalDamage;
-                player.shootingRate += additionalShootingRate;
+                player.shootingRate += Mathf.Clamp(player.shootingRate + additionalShootingRate, 0.1f, 1);
 
                 Destroy(this.gameObject);
             }
